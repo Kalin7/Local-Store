108f3cfd3c2a8795b31850942abf82e7b1f75b6e
Kalin7 <kalin.kirov7@gmail.com>
2022-07-11 11:14:47 +0300

    Register form created

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faAt, faLock, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    library: FaIconLibrary,
+  ) { 
+    library.addIcons(
+      faAt,
+      faLock,
+      faPhone
+    )
+  }
 
   ngOnInit(): void {
   }
